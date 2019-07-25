e1f81e3f820d97904c2140c6e634940a7344ef85
VagnerDomingues <vagner_domingues@hotmail.com>
2019-07-25 04:42:54 -0300

    Improve code quality

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # jquery.table-shrinker
+
 A fast-render, lightweight plugin to turn tables responsive the right way.
 <br/>
 
@@ -6,44 +7,51 @@ A fast-render, lightweight plugin to turn tables responsive the right way.
 
 ## Why should you use it?
 
-* Highly customizable page-breaks.
-* Won't break your table's primordial design, it uses css only to re-structure table for mobile.
-* Designed to attend fat-fingers concept to prevent missclicking, you can tap anywhere in the row and it will trigger the collapser.
-* You can maintain all previous functions working anywhere inside the elements of the table. (ie: a dropdown in table headers)
-* Couldn't find another plugin in the web which support chained tables. (ie: tables within tables)
-* It's free, lightweight and don't rely in any major js lib other than jquery.
-
+-   Highly customizable page-breaks.
+-   Won't break your table's primordial design, it uses css only to re-structure table for mobile.
+-   Designed to attend fat-fingers concept to prevent missclicking, you can tap anywhere in the row and it will trigger the collapser.
+-   You can maintain all previous functions working anywhere inside the elements of the table. (ie: a dropdown in table headers)
+-   Couldn't find another plugin in the web which support chained tables. (ie: tables within tables)
+-   It's free, lightweight and don't rely in any major js lib other than jquery.
 
 ## What are its limitations?
 
-* It does not support irregular headers. (ie: vertical headers)
-* It does not support multiline headers.
-* It does not support colspan attr.
+-   It does not support irregular headers. (ie: vertical headers)
+-   It does not support multiline headers.
+-   It does not support colspan attr.
 
 ### Prerequisites
+
 [Jquery](https://jquery.com/)
+
 ```html
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" crossorigin="anonymous"></script>
 ```
 
 ### Installing
+
 Insert the .css
+
 ```html
 <link rel="stylesheet" href="jquery.table-shrinker.min.css">
 ```
 
 Insert the .js
+
 ```html
 <script src="jquery.table-shrinker.min.js"></script>
 ```
 
 ## How to use
+
 First, add <b>shrink</b> class to the table you want to be shrinked
 
 ```html
   <table class="shrink">
 ```
+
 Next, start the plugin right after the DOM finished rendering.
+
 ```javascript
   options = {
         useZebra: false     // i don't like zebras
@@ -51,12 +59,15 @@ Next, start the plugin right after the DOM finished rendering.
 
   $("table.shrink").tableShrinker(options)
 ```
+
 If you are designing a table that will have another table that uses the tableShrinker inside its cells, you should call chained() method before the initializer.
+
 ```javascript
   $("table.shrink").chained().tableShrinker(options)
 ```
 
 And finally, add <b>shrink-XX</b> classes to the header to tell them when you want them to be shrinked
+
 ```html
 <thead>
   <tr>
@@ -73,70 +84,102 @@ And finally, add <b>shrink-XX</b> classes to the header to tell them when you wa
 
 Additionally, there's a <b>shrinkable</b> class which make texts containers responsive
 This is usefull if you like to keep displaying a large info cell in a tiny devices, preventing its texts to breakline.
-``` html
+
+```html
   <th class="shrinkable"> Full Name </th>
 ```
 
----
+* * *
+
 Basically that's it. your table should be shrinked by now.
 <br/>
 Below there's the options explanation and some hints of use.
 
----
+* * *
+
 ### Options
 
 #### useZebra
+
 ###### default: true
+
 Table rows inside wrapper became striped.
+
 #### useTransitions
+
 ###### default: true
+
 Animate when collapsing the wrapper.
+
 #### transitionSpeed
+
 ###### default: 300
+
 name says it all.
+
 #### ignoreWhenHit
+
 ###### default: 'input, button, a, .btn'
+
 If you have a customized radiobutton or selectbox on each row you might want to ignore them too. you can add then to this selector.
 <br>
 so if any tap/click event hit any of these elements it's event propagation is stoped, and they keep their functionality inside the shrinked table container.
+
 #### showToggle
+
 ###### default: true
+
 You can disable the toggle icon and still show/hide the shrinked content by hiting the anywhere in the row (except for the ignoreWhenHit elements).
+
 #### customToggle
+
 ###### default: ['\<span\>˅\<\/span\>','\<span\>˄\<\/span\>]
+
 A list of two html's that alternate when wrapper collapses, you can add classes or change it's content and control the toggle's design using your own css rules.
+
 #### showToggleAll
+
 ###### default: true
+
 You can disable the toggle icon and still show/hide the shrinked content by hiting the anywhere in the row (except for the ignoreWhenHit elements).
+
 #### customToggleAll
+
 ###### default: ['\<span\>˅\<\/span\>','\<span\>˄\<\/span\>]
+
 A list of two html's that alternate when all wrapper collapses, you can add classes or change it's content and control the toggle's design using your own css rules.
+
 #### loadCollapsed
+
 ###### default: null
+
 If true, forces all shrinked elements to be visible on window first load, else it will look for load-collapsed classes for each table separately.
 
-
-
 ### Quick Tips
+
 #### Classname Property Controls
+
 You can add "shrink-[property-name]" classes to each individual table to control their behaviour separately, this way you can call the plugin constructor just once. just remember to not override their defaults on options array.
 <br>
-- shrink-use-zebra
-- shrink-show-toggle-all
-- shrink-show-toggle
-- shrink-load-collapsed
-For example:
+
+-   shrink-use-zebra
+-   shrink-show-toggle-all
+-   shrink-show-toggle
+-   shrink-load-collapsed
+    For example:
+
 ```html
       <table class="shrink shrink-load-collapsed  ">
           (...)
       </table>
 ```
 
-
 #### Instant Load
+
 The table can be instantly rendered at the first graphic engine loop of the browser by adding the same <b>shrink-XX</b> and <b>shrinkable</b> classes that you used on the table headers to every table cell
 <br>
 For example:
+
 ```html
 <thead>
   <tr>
@@ -159,12 +202,13 @@ For example:
   (...)
 </tbody>
 ```
+
 This way your table first loads directly in it's shrinked state!
 <br/>
 There's no impact in performance speed, the only downside of this is that if you have tables that renders 100+ rows in the same page your DOM tree can get very large.
 
-
 #### Larger than desktops
+
 If you want to show more data in the same table but even desktops width can't handle, just keep adding columns with <b>shrink-xl</b>
 
 ```html
@@ -174,11 +218,13 @@ If you want to show more data in the same table but even desktops width can't ha
   </tr>
 </thead>
 ```
+
 <br>
 
-
 #### Blank Header
+
 You can use a blank header to add a full-width column, perphaps you want to shrink just a single button.. in this case:
+
 ```html
 <thead>
   <tr>
@@ -198,8 +244,8 @@ You can use a blank header to add a full-width column, perphaps you want to shri
 </tbody>
 ```
 
-
 #### Collapsable Chained Tables
+
 To add a collapsable table row you can combine the previous 'XL' hint and the chained() method.
 
 ```html
--- jquery.table-shrinker.js
+++ jquery.table-shrinker.js
@@ -1,15 +1,15 @@
 var resizeEvent;
 jQuery.fn.chained = [].reverse;
-(function ($, window, document, undefined) {
+(function ($, window, document) {
     "use strict";
     var pluginName = "tableShrinker",
         defaults = {
             useZebra: true,
             useTransitions: true,
             transitionSpeed: 300,
-            ignoreWhenHit: 'input, button, a, .btn',
-            customToggle: ['<span>˅</span>','<span>˄</span>'],
-            customToggleAll: ['<span>˅</span>','<span>˄</span>'],
+            ignoreWhenHit: "input, button, a, .btn",
+            customToggle: ["<span>˅</span>","<span>˄</span>"],
+            customToggleAll: ["<span>˅</span>","<span>˄</span>"],
             showToggle: false,
             showToggleAll: false,
             loadCollapsed: null
@@ -33,89 +33,89 @@ jQuery.fn.chained = [].reverse;
             let _this = this;
             let _toggle = _this.settings.customToggle;
             let _toggleAll = _this.settings.customToggleAll;
-            let _useZebra = _this.settings.useZebra == true ? true : $(_this.element).hasClass('shrink-use-zebra');
-            let _showToggle = _this.settings.showToggle == true ? true : $(_this.element).hasClass('shrink-show-toggle');
-            let _showToggleAll = _this.settings.showToggleAll == true ? true : $(_this.element).hasClass('shrink-show-toggle-all');
-            let _loadCollapsed = _this.settings.loadCollapsed == true ? true : $(_this.element).hasClass('shrink-load-collapsed')
-            let _suffixes = '';
+            let _useZebra = _this.settings.useZebra == true ? true : $(_this.element).hasClass("shrink-use-zebra");
+            let _showToggle = _this.settings.showToggle == true ? true : $(_this.element).hasClass("shrink-show-toggle");
+            let _showToggleAll = _this.settings.showToggleAll == true ? true : $(_this.element).hasClass("shrink-show-toggle-all");
+            let _loadCollapsed = _this.settings.loadCollapsed == true ? true : $(_this.element).hasClass("shrink-load-collapsed");
+            let _suffixes = "";
             let _headerRow = null;
 
             // instance properties
             _this.$t = $(_this.element);
-            _this.$ths = _this.$t.children('thead').first().find('> tr > th');
-            _this.$trs = _this.$t.children('tbody').first().find('> tr');
-            _this.transitionSpeed = _this.settings.useTransitions == true ? _this.settings.transitionSpeed : 0;
+            _this.$ths = _this.$t.children("thead").first().find("> tr > th");
+            _this.$trs = _this.$t.children("tbody").first().find("> tr");
+            _this.transitionSpeed = _this.settings.useTransitions === true ? _this.settings.transitionSpeed : 0;
 
-            if(_useZebra)_this.$t.addClass('shrink-use-zebra')
+            if(_useZebra){_this.$t.addClass("shrink-use-zebra")};
 
             // start shrinkable restructure
             _this.$trs.each(function (rId){
-                let r = $(this).addClass('shrink-row').after('<tr class="blank-row"></tr>').after('<tr class="shrink-wrapper"><td colspan="99"></td></tr>');
+                let r = $(this).addClass("shrink-row").after("<tr class=\"blank-row\"></tr>").after("<tr class=\"shrink-wrapper\"><td colspan=\"99\"></td></tr>");
                 _this.$ths.each(function (hId) {
-                    if(r.children('td').attr('colspan') != null) return//ignore if has colspan
-                    if($(this)[0].className.match('shrinkable')) r.find('td').eq(hId).addClass('shrinkable')
-                    let re = new RegExp('(?:shrink-)([a-z]*)[^ ]?');
+                    if(r.children("td").attr("colspan") != null){return}//ignore if has colspan
+                    if($(this)[0].className.match("shrinkable")){r.find("td").eq(hId).addClass("shrinkable")};
+                    let re = new RegExp("(?:shrink-)([a-z]*)[^ ]?");
                     let result;
-                    if(r.parents('table').first().find('th').eq(hId)[0]){
-                        result = r.parents('table').first().find('th').eq(hId)[0].className.match(re);
+                    if(r.parents("table").first().find("th").eq(hId)[0]){
+                        result = r.parents("table").first().find("th").eq(hId)[0].className.match(re);
                     }
                     if (result){
 
                         //setup shrink/unshrinked elements
-                        if(r.find('td').eq(hId).parents('table').first() != null){
+                        if(r.find("td").eq(hId).parents("table").first() != null){
 
-                            _suffixes = _suffixes + ' ' + result[1];
+                            _suffixes = _suffixes + " " + result[1];
 
-                            if(r.parents('table').first().find('th').eq(hId).html().trim() == '')
-                                r.next('.shrink-wrapper').find('td').append('<div class="shrinked-row" style="display:none"><div class="unshrink-' + result[1] + '"><div style="width:100%">' + r.find('td').eq(hId).html() + '</div></div></div>')
-                            else
-                                r.next('.shrink-wrapper').find('td').append('<div class="shrinked-row" style="display:none"><div class="unshrink-' + result[1] + '"><div>' + r.parents('table').first().find('th').eq(hId).html() + '</div><div>' + r.find('td').eq(hId).html() + '</div></div></div>')
-
-                            r.children('td').eq(hId).addClass('shrink-'+ result[1])
-                        }
+                            if(r.parents("table").first().find("th").eq(hId).html().trim() === ""){
+                                r.next(".shrink-wrapper").find("td").append("<div class=\"shrinked-row\" style=\"display:none\"><div class=\"unshrink-" + result[1] + "\"><div style=\"width:100%\">" + r.find("td").eq(hId).html() + "</div></div></div>");
+                            }else{
+                                r.next(".shrink-wrapper").find("td").append("<div class=\"shrinked-row\" style=\"display:none\"><div class=\"unshrink-" + result[1] + "\"><div>" + r.parents("table").first().find("th").eq(hId).html() + "</div><div>" + r.find("td").eq(hId).html() + "</div></div></div>");
+                            };
+                            r.children("td").eq(hId).addClass("shrink-"+ result[1]);
+                        };
 
                         //setup toggle buttons
-                        if(hId == r.children('td').last().index() && r.children('td:not(:has(table))').length > 0){
+                        if(hId === r.children("td").last().index() && r.children("td:not(:has(table))").length > 0){
 
                             //setup toggle
-                            if(_showToggle) r.append('<td class="shrink-toggle">'+ _toggle[0] +'</td>')
-                            else  r.append('<td class="shrink-toggle"></td>')
-                            _suffixes.trim().split(" ").forEach(function (val){r.find('td.shrink-toggle').addClass('unshrink-' + val)})
+                            if(_showToggle){r.append("<td class=\"shrink-toggle\">"+ _toggle[0] +"</td>");}
+                            else{r.append("<td class=\"shrink-toggle\"></td>");}
+                            _suffixes.trim().split(" ").forEach(function (val){r.find("td.shrink-toggle").addClass("unshrink-" + val)});
 
                             //setup toggleAll
-                            if(rId == 0 ){
-                                _headerRow = r.parents('table').first().children('thead').children('tr');
-                                if(_showToggleAll) _headerRow.append('<th class="shrink-toggle-all">' + _toggleAll[0] + '</th>');
-                                else _headerRow.append('<th class="shrink-toggle-all hidden"></th>');
-                                _suffixes.trim().split(" ").forEach(function (val){_headerRow.find('th.shrink-toggle-all').addClass('unshrink-' + val)})
-                            }
-                        }
-                    }
-                })
+                            if(rId === 0 ){
+                                _headerRow = r.parents("table").first().children("thead").children("tr");
+                                if(_showToggleAll){_headerRow.append("<th class=\"shrink-toggle-all\">" + _toggleAll[0] + "</th>");}
+                                else{_headerRow.append("<th class=\"shrink-toggle-all hidden\"></th>");}
+                                _suffixes.trim().split(" ").forEach(function (val){_headerRow.find("th.shrink-toggle-all").addClass("unshrink-" + val)});
+                            };
+                        };
+                    };
+                });
             });
-            $('.shrinked-row:has(table)').addClass('shrink-has-table');
+            $(".shrinked-row:has(table)").addClass("shrink-has-table");
 
             // Bind events
-            _this.$t.children('thead').first().children('tr').on("click", _this.toggleAll.bind(_this)); // toggle-all for main table
+            _this.$t.children("thead").first().children("tr").on("click", _this.toggleAll.bind(_this)); // toggle-all for main table
 
-            _this.$t.find('.shrinked-row.shrink-has-table table>thead>tr').on("click", _this.toggleAll.bind(_this)); // toggle-all for shrinked tables
+            _this.$t.find(".shrinked-row.shrink-has-table table>thead>tr").on("click", _this.toggleAll.bind(_this)); // toggle-all for shrinked tables
 
-            if(_this.$t.parents('table').length == 0){_this.$t.children('tbody').first().children('tr').on("click", _this.toggle.bind(_this));}
+            if(_this.$t.parents("table").length === 0){_this.$t.children("tbody").first().children("tr").on("click", _this.toggle.bind(_this));};
 
-            if (_loadCollapsed) _this.$t.find('>thead>tr').click();
+            if (_loadCollapsed) _this.$t.find(">thead>tr").click();
         },
         toggle: function (e){
             if ($(e.target).is(this.settings.ignoreWhenHit)){
                 return
             }
-            if (window.getSelection().type != "Range" && !$(e.target).parents('table').first().find('.shrinked-row').is(':animated')){
-                let nextWrapper = $(e.target).closest('tr').next('.shrink-wrapper');
+            if (window.getSelection().type != "Range" && !$(e.target).parents("table").first().find(".shrinked-row").is(":animated")){
+                let nextWrapper = $(e.target).closest("tr").next(".shrink-wrapper");
 
-                setTimeout(this.updateToggle(nextWrapper, nextWrapper.find('td>div.shrinked-row > div:visible').length > 0 ? 0 : 1), this.transitionSpeed);
+                setTimeout(this.updateToggle(nextWrapper, nextWrapper.find("td>div.shrinked-row > div:visible").length > 0 ? 0 : 1), this.transitionSpeed);
 
-                nextWrapper.find('>td>div.shrinked-row').slideToggle(this.transitionSpeed);
+                nextWrapper.find(">td>div.shrinked-row").slideToggle(this.transitionSpeed);
 
-                if($(e.target).parents('table').first().hasClass('shrink-use-zebra')) updateZebra();
+                if($(e.target).parents("table").first().hasClass("shrink-use-zebra")) updateZebra();
 
                 setTimeout(function(){ updateCollapsed()}, this.transitionSpeed);
             };
@@ -124,33 +124,33 @@ jQuery.fn.chained = [].reverse;
             if ($(e.target).is(this.settings.ignoreWhenHit)){
                 return
             }
-            if (window.getSelection().type != "Range" && !$(e.target).parents('table').first().find('.shrinked-row').is(':animated')){
-                let currentTable = $(e.target).parents('table').first();
+            if (window.getSelection().type != "Range" && !$(e.target).parents("table").first().find(".shrinked-row").is(":animated")){
+                let currentTable = $(e.target).parents("table").first();
 
-                currentTable.toggleClass('shrink-collapsed')
+                currentTable.toggleClass("shrink-collapsed");
 
-                let isAlreadyCollapsed = currentTable.hasClass('shrink-collapsed') ? 1 : 0;
+                let isAlreadyCollapsed = currentTable.hasClass("shrink-collapsed") ? 1 : 0;
 
                 setTimeout(this.updateToggleAll(currentTable, isAlreadyCollapsed), this.transitionSpeed);
 
-                if(isAlreadyCollapsed) currentTable.find('>tbody>tr.shrink-wrapper>td>div.shrinked-row:hidden').slideDown(this.transitionSpeed, function(){})
-                else currentTable.find('>tbody>tr.shrink-wrapper>td>div.shrinked-row:visible').slideUp(this.transitionSpeed, function(){});
+                if(isAlreadyCollapsed) currentTable.find(">tbody>tr.shrink-wrapper>td>div.shrinked-row:hidden").slideDown(this.transitionSpeed, function(){});
+                else currentTable.find(">tbody>tr.shrink-wrapper>td>div.shrinked-row:visible").slideUp(this.transitionSpeed, function(){});
 
-                if($(e.target).parents('table').first().hasClass('shrink-use-zebra')) updateZebra();
+                if($(e.target).parents("table").first().hasClass("shrink-use-zebra")) updateZebra();
 
                 setTimeout(function(){ updateCollapsed()}, this.transitionSpeed);
             }
         },
         updateToggle : function(wrapper, toggleState){
-            let _showToggle = this.settings.showToggle == true ? true : $(this.element).hasClass('shrink-show-toggle');
-            if(_showToggle) wrapper.prev('tr').children('.shrink-toggle').first().html(this.settings.customToggle[toggleState]);
+            let _showToggle = this.settings.showToggle == true ? true : $(this.element).hasClass("shrink-show-toggle");
+            if(_showToggle) wrapper.prev("tr").children(".shrink-toggle").first().html(this.settings.customToggle[toggleState]);
         },
         updateToggleAll : function(table, toggleState){
-            let _showToggleAll = this.settings.showToggleAll == true ? true : table.hasClass('shrink-show-toggle-all');
-            let _showToggle = this.settings.showToggle == true ? true : table.hasClass('shrink-show-toggle');
+            let _showToggleAll = this.settings.showToggleAll == true ? true : table.hasClass("shrink-show-toggle-all");
+            let _showToggle = this.settings.showToggle == true ? true : table.hasClass("shrink-show-toggle");
 
-            if(_showToggleAll) table.find('>thead>tr>th.shrink-toggle-all').first().html(this.settings.customToggleAll[toggleState]);
-            if(_showToggle) table.find('>tbody>tr>td.shrink-toggle:not(.hidden)').html(this.settings.customToggle[toggleState]);
+            if(_showToggleAll) table.find(">thead>tr>th.shrink-toggle-all").first().html(this.settings.customToggleAll[toggleState]);
+            if(_showToggle) table.find(">tbody>tr>td.shrink-toggle:not(.hidden)").html(this.settings.customToggle[toggleState]);
         }
     });
     // A lightweight plugin wrapper around the constructor, preventing against multiple instantiations
@@ -158,32 +158,32 @@ jQuery.fn.chained = [].reverse;
         return this.each(function () {
             if (!$.data(this, "plugin-" + pluginName)) {
                 $.data(this, "plugin-" + pluginName, new Plugin(this, options));
-            }
+            };
         });
     };
 
-})(jQuery, window, document);
+}(jQuery, window, document));
 
 
 function updateCollapsed(){
-    $('tr.shrink-row + tr.shrink-wrapper:visible').each(function(id, e){
-        if($(e).height()) $(e).prev('tr').addClass('collapsed')
-        else $(e).prev('tr').removeClass('collapsed')
+    $("tr.shrink-row + tr.shrink-wrapper:visible").each(function(id, e){
+        if($(e).height()) $(e).prev("tr").addClass("collapsed")
+        else $(e).prev("tr").removeClass("collapsed")
     });
 };
 
 function updateZebra(){
     setTimeout(function(){
-        $('.shrink.shrink-use-zebra tr.shrink-row + tr.shrink-wrapper:visible').each(function(){
-            $(this).find('> td > div.shrinked-row:not(:has(table)) div:visible').each(function (i) {
-                $(this).parent().removeClass('even odd');
-                $(this).parent().addClass(i % 2 == 0 ? 'even': 'odd');
+        $(".shrink.shrink-use-zebra tr.shrink-row + tr.shrink-wrapper:visible").each(function(){
+            $(this).find("> td > div.shrinked-row:not(:has(table)) div:visible").each(function (i) {
+                $(this).parent().removeClass("even odd");
+                $(this).parent().addClass(i % 2 === 0 ? "even": "odd");
             });
-        })
+        });
     }, 300);
 };
 
-$(window).on('resize', function(){
-    updateZebra()
-    updateCollapsed()
+$(window).on("resize", function(){
+    updateZebra();
+    updateCollapsed();
 });
